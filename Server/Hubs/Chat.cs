4c76dfc0b468f33c09bda323bf4bf0a7ecae0a69
Ezra Mallo <ezra.c.mallo@gmail.com>
2024-03-21 06:33:57 -0600

    Add project files.

---
+++ Server/Hubs/Chat.cs
@@ -0,0 +1,11 @@
+﻿namespace emalloLab4.Model;
+
+public class Chat
+{
+    public int? ID { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? CategoryOfTheComplaint{ get; set; }
+    public string? ComplaintMessage { get; set; }
+    public DateTime? CreatedDate { get; set; }
+}
